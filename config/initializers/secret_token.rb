b98b96649baef314cfe3e41281398099c14e7b7f
Dmitry Alexeenko <dalexeenko@gmail.com>
2013-06-08 14:53:38 -0700

    First checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sydekick::Application.config.secret_token = '6cdb4d639a96a85f879a3d335c97e11d71ee440775a40fb78b58cf778f23474b17d28851be9aec54c81cbcc8ead1a7db6465be9645a9afa6d5da143305732816'
